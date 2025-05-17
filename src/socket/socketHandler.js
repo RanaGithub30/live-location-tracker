ea993a811c008d44d23fde7bacc1a9556bee12f2
Rana Saha <ranasaha03edu@gmail.com>
2025-05-17 19:02:14 +0530

    Add a navbar & Use Templating

--- src/socket/socketHandler.js
+++ src/socket/socketHandler.js
@@ -1,7 +1,5 @@
 module.exports = function(io) {
     io.on("connection", (socket) => {
-        console.log(`User connected: ${socket.id}`);
-
         socket.on("location", (data) => {
             io.emit("receive-location", { id: socket.id, ...data });
         });
